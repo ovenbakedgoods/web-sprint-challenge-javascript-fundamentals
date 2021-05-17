12ba49d6ec0f914e03acec2e6850234e6829062d
ovenbakedgoods <ovenbakedgoods88@gmail.com>
2021-05-16 22:09:11 -0500

    Finishing up this keyword questions on readme

--- index.js
+++ index.js
@@ -240,7 +240,10 @@ class CuboidMakerTwo
 
 
 // 🦄 💪 Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area. 🦄 💪
-  
+  /*class CubeMaker extends CuboidMaker(attribs)
+  {
+
+  }*/
 
 
 
